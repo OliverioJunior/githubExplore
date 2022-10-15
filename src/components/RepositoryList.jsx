d61e59c51f1ef8d27f32e319635f793fd476d3e9
Oliverio <oliverio.junior2@gmail.com>
2022-10-15 10:01:45 -0300

    Criando primeiro component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,34 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositorios</h1>
+
+      <ul>
+        <li>
+          <strong>unforme</strong>
+          <p>Form in ReactJs</p>
+
+          <a href='#'>
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unforme</strong>
+          <p>Form in ReactJs</p>
+
+          <a href='#'>
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unforme</strong>
+          <p>Form in ReactJs</p>
+
+          <a href='#'>
+            Acessar repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}

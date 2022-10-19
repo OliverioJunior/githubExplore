8d30375a09c51468d0ff40ebfb26f7db4fb28599
Oliverio <oliverio.junior2@gmail.com>
2022-10-19 06:00:33 -0300

    Finalizar projeto

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,10 +1,10 @@
-import { RepositoryItem } from "./RepositoryItem";
+import { RepositoryItem, RepositoryItemProps } from "./RepositoryItem";
 import '../style/repositories.scss';
 import { useEffect, useState } from "react";
 //https://api.github.com/users/OliverioJunior/repos
 
 export function RepositoryList() {
-  const [repo, setRepo] = useState([]);
+  const [repo, setRepo] = useState<RepositoryItemProps[]>([]);
 
   useEffect(()=>{
     fetch('https://api.github.com/users/OliverioJunior/repos')
@@ -16,7 +16,7 @@ export function RepositoryList() {
       <h1>Lista de repositorios</h1>
 
       <ul>
-        {repo.map(repos => <RepositoryItem key={repos.name} repository= {repos} />)}
+        {repo.map(repos => <RepositoryItem key={repos.repository.name} repository={repos.repository} />)}
     
 
       </ul>
